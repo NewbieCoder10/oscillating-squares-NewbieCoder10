7434c4c922134669f5f164223b1991aa344d25a6
Khasiano <NewbieCoder10>
2019-10-11 12:20:40 -0500

    Initial commit for Oscillating squares.

--- src/edu/cnm/deepdive/Generator.java
+++ src/edu/cnm/deepdive/Generator.java
@@ -1,5 +1,8 @@
 package edu.cnm.deepdive;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  * This class contains a stub of the {@link #oscillatingSquares(int)} method. Implementation of this
  * method is included as a practical exam problem of the Deep Dive Coding Java + Android Bootcamp.
@@ -27,7 +30,36 @@ public class Generator {
    * @return generated array of oscillating squares.
    */
   public static int[] oscillatingSquares(int length) {
-    return null; // TODO Replace this line with implementation.
+
+
+       /*  This applet draws a red-and-black checkerboard.
+           It is assumed that the size of the applet is 160
+           by 160 pixels.
+       */
+
+    public void paint(Graphics Graphics g) {
+
+      int row;   // Row number, from 0 to 7
+      int col;   // Column number, from 0 to 7
+      int x,y;   // Top-left corner of square
+
+      for ( row = 0;  row < 8;  row++ ) {
+
+        for ( col = 0;  col < 8;  col++) {
+          x = col * 20;
+          y = row * 20;
+          if ( (row % 2) == (col % 2) )
+            g.setColor(Color.red);
+          else
+            g.setColor(Color.black);
+          g.fillRect(x, y, 20, 20);
+        }
+
+      }
+
+    }
+
+  }
+    // TODO Replace this line with implementation.
   }
 
-}
---
+++ src/edu/cnm/deepdive/OscillatingSquaresTest.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OscillatingSquaresTest {
+
+  @org.junit.jupiter.api.Test
+  void oscillatingSquares() {
+  }
+}
